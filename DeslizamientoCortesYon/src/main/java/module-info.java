1c15a919f0f96576ff02bb9922e32711885f05bf
ycorber487 <163828691+ycorber487@users.noreply.github.com>
2024-12-10 16:14:09 +0100

    Delete DeslizamientoCortesYon directory

--- DeslizamientoCortesYon/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ies.montecillos.deslizamientocortesyon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ies.montecillos.deslizamientocortesyon to javafx.fxml;
-    exports ies.montecillos.deslizamientocortesyon;
-}
